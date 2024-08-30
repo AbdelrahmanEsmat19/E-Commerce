2cf3280be16d7adfc26c1316f0b7c8467c02f67e
Essmat <babdo0196@gmail.com>
2024-08-30 06:08:29 +0300

    Not

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react'
-import classes from "./NotFound.module.css"
+import React from 'react';
+import classes from "./NotFound.module.css";
+
 export default function NotFound() {
   return (
-
-    <>
-    <h1>NotFound</h1>
-    </>
-  )
+    <div className={`${classes.notFoundContaine} h-screen flex items-center justify-center flex-col`}>
+      <i class="fa-solid fa-triangle-exclamation text-red-600 fa-4x text-end"></i>
+      <h1 className={classes.heading}>404</h1>
+      <p className={classes.description}>Oops! The page you're looking for doesn't exist.</p>
+    </div>
+  );
 }
