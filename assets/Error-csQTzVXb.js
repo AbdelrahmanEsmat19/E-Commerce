1adf69eb8b40cdb357fa23990249627ea6361412
Essmat <babdo0196@gmail.com>
2024-08-30 05:46:00 +0300

    Updates

---
+++ assets/Error-csQTzVXb.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-dk4rlL9Q.js";import{H as e}from"./Helmet-Cz8JCpZD.js";function o(){return r.jsxs(r.Fragment,{children:[r.jsx("h1",{children:"Error"}),r.jsxs(e,{children:[r.jsx("meta",{charSet:"utf-8"}),r.jsx("title",{children:"Error Page"}),r.jsx("link",{rel:"canonical",href:"http://mysite.com/example"})]})]})}export{o as default};
---
+++ assets/NotFound-CvhcQy2g.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-dk4rlL9Q.js";function o(){return t.jsx(t.Fragment,{children:t.jsx("h1",{children:"NotFound"})})}export{o as default};
