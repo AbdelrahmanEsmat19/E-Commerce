748e4f5e6bdea6a94df973ee47e395f543448a3b
Essmat <babdo0196@gmail.com>
2024-08-30 06:26:26 +0300

    Updates

---
+++ assets/Error-DzRtgc6m.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Cy0O8hH2.js";import{H as e}from"./Helmet-D1ymkz29.js";function o(){return r.jsxs(r.Fragment,{children:[r.jsx("h1",{children:"Error"}),r.jsxs(e,{children:[r.jsx("meta",{charSet:"utf-8"}),r.jsx("title",{children:"Error Page"}),r.jsx("link",{rel:"canonical",href:"http://mysite.com/example"})]})]})}export{o as default};
