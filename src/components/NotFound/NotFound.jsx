5022ff4a575c30868b030b13369d33e50868de83
Essmat <babdo0196@gmail.com>
2024-09-11 22:32:55 +0300

    not

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -3,10 +3,10 @@ import classes from "./NotFound.module.css";
 
 export default function NotFound() {
   return (
-    <div className={`${classes.notFoundContaine} h-screen flex items-center justify-center flex-col`}>
+    <div className={`${classes.notFoundContaine}  h-screen flex items-center justify-center flex-col`}>
       <i class="fa-solid fa-triangle-exclamation text-red-600 fa-4x text-end"></i>
-      <h1 className={classes.heading}>404</h1>
-      <p className={classes.description}>Oops! The page you're looking for doesn't exist.</p>
+      <h1 className={`${classes.heading} dark:text-white`}>404</h1>
+      <p className={`${classes.description} dark:text-white`}>Oops! The page you're looking for doesn't exist.</p>
     </div>
   );
 }
