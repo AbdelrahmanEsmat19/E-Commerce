5c6f4f25a719d4094ebb646d2d71da4273645ba7
Essmat <babdo0196@gmail.com>
2024-09-13 16:23:14 +0300

    Nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -42,18 +42,18 @@ function Navbar() {
   }, [darkMode]);
 
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center lg:justify-between mx-auto lg:p-0 p-2 ">
+    <nav className="bg-white dark:bg-gray-900 fixed  w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-evenly lg:justify-between mx-auto p-2 lg:p-4">
         <Link
           to="/"
-          className={"flex items-center space-x-1 lg:space-x-3 rtl:space-x-reverse"}
+          className={"flex items-center space-x-1 rtl:space-x-reverse"}
         >
           <img src="./favicon.png" className="h-8" alt="Fresh Cart Logo" />
-          <span className="self-center text-2xl m-9 font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             FreshCart
           </span>
         </Link>
-        <div className="flex md:order-2 items-center  md:space-x-0 rtl:space-x-reverse">
+        <div className="flex md:order-2 space-x-1 items-center md:space-x-0 rtl:space-x-reverse">
           <ul className="flex flex-col p-2 md:p-0 me-8 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:text-white dark:border-gray-700">
             {userToken ? (
               <>
